850d1a7dbfb30b1eb3dc9b763c65f4c541933790
IlBennys <119436512+IlBennys@users.noreply.github.com>
2022-12-05 14:36:30 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let nome = "benedetto";
+console.log(nome);
+
+let numero1 = 12;
+let numero2 = 20;
+console.log(numero1 + numero2);
+console.log((numero1 += numero2));
+
+let x = 12;
+console.log(x);
