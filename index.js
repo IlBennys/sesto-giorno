ddad17e684c2db03e8082479edcabdd8c05300ba
IlBennys <119436512+IlBennys@users.noreply.github.com>
2022-12-05 15:06:48 +0100

    ok fin

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-let nome = "benedetto";
-console.log(nome);
+let numero1 = 12
+let numero2 = 20
+console.log(numero1 + numero2)
+console.log((numero1 += numero2))
 
-let numero1 = 12;
-let numero2 = 20;
-console.log(numero1 + numero2);
-console.log((numero1 += numero2));
+let x = 12
+console.log(x)
 
-let x = 12;
-console.log(x);
-ok;
+var nome = "benedetto"
+let cognome = "Manfrè"
+console.log(cognome)
+
+const name12 = "Benny"
+console.log(name12)
+
+let numberfour = 4
+console.log(x - numberfour)
+
+let name1 = "john"
+let name2 = "John"
+console.log(name1 === name2)
+
+console.log(name1 === name2.toLowerCase())
